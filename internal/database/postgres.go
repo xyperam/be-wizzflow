603cbda03670b96e590fb824ab8aa7d318c77daa
Ilham Imani Nur Alam <ilhamimaninuralam@gmail.com>
2026-03-03 23:20:32 +0700

    feat : implement postgres repo

---
+++ internal/database/postgres.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitDB() (*pgxpool.Pool, error) {
+	dsn := "postgres://user:password@localhost:5432/wizzflow"
+
+	config, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, err
+	}
+
+	// check connection
+	err = pool.Ping(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to PostgreSql")
+	return pool, nil
+}
--- internal/repository/transaction_repository_test.go
+++ internal/repository/transaction_repository_test.go
@@ -1,22 +1,59 @@
 package repository
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/xyperam/wizzflow/internal/models"
+)
 
 func TestDeleteTransaction(t *testing.T) {
-	repo := NewRepository()
-
-	err := repo.DeleteTransaction(1)
+	// Gunakan DSN yang sama dengan aplikasi jika belum buat db khusus test
+	ctx := context.Background()
+	dsn := "postgres://user:password@localhost:5432/wizzflow"
+	pool, err := pgxpool.New(ctx, dsn)
 	if err != nil {
-		t.Errorf("Harusnya suskse hapus ID 1, tapi dapat error %v", err)
+		t.Fatalf("failed to create pool: %v", err)
+	}
+	defer pool.Close()
+
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		t.Fatalf("Failed to begin transaction %v", err)
+	}
+	defer tx.Rollback(ctx)
+	repo := NewRepository(pool)
+
+	// Langkah 1: Insert data dulu buat bahan hapusan
+	dummy := models.Transaction{Title: "Test", Amount: 100, Type: "expense", Category: "test"}
+	created, err := repo.SaveTransaction(dummy)
+	if err != nil {
+		t.Fatalf("Gagal bikin data dummy: %v", err)
 	}
 
-	transactions := repo.FindAll()
-	if len(transactions) != 0 {
-		t.Errorf("Harusnya slice kosong tapi sisa %d", len(transactions))
+	// Langkah 2: Test Hapus yang beneran ada
+	err = repo.DeleteTransaction(created.ID) // Gunakan ID yang baru dibuat
+	if err != nil {
+		t.Errorf("Harusnya sukses hapus ID %d, tapi dapat error: %v", created.ID, err)
 	}
 
-	err = repo.DeleteTransaction(1)
+	// Langkah 3: Cek apakah benar-benar hilang
+	transactions, err := repo.FindAll()
+	if err != nil {
+		t.Fatalf("Gagal FindAll: %v", err)
+	}
+
+	// Pastikan ID yang tadi dihapus sudah tidak ada di list
+	for _, item := range transactions {
+		if item.ID == created.ID {
+			t.Errorf("ID %d masih ada di database!", created.ID)
+		}
+	}
+
+	// Langkah 4: Test Hapus ID yang TIDAK ADA (Expect Error)
+	err = repo.DeleteTransaction(999999)
 	if err == nil {
-		t.Errorf("Harusnya id 1 sudah dihapus tapi malah nil")
+		t.Error("Harusnya error karena ID 999999 tidak ada, tapi malah nil")
 	}
 }
